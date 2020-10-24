1ee225eacfe37bf60a1bcd479c173b0579256693
ries9112 <info@predictcrypto.net>
2020-10-23 19:08:39 -0600

    initial commit of files

---
+++ add_to_lockfile.R
@@ -0,0 +1,8 @@
+# Add to lockfile
+lock <- renv:::lockfile("renv.lock")
+# set the repositories for a lockfile
+lock$repos(CRAN = "https://cran.r-project.org")
+# depend on digest 0.6.22
+lock$add(pacman = "pacman@0.5.1")
+# write to file
+lock$write("renv.lock")
